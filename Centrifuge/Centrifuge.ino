74655dde1d5dafd4aa128940d56efd767d357748
Ethan Li <lietk12@gmail.com>
2020-11-23 19:16:03 -0800

    Add adaptive sampling interval for lower noise at high RPM

--- Centrifuge/Centrifuge.ino
+++ Centrifuge/Centrifuge.ino
@@ -8,11 +8,10 @@
 #include <Ewma.h>
 #include <EwmaT.h>
 
-unsigned long current_time = 0;
-unsigned long blink_period = 100;
-bool blink_state = false;
-unsigned long blink_start_time = 0;
+// Timing
+unsigned long current_time;
 
+// Motor
 static const uint8_t motor_pin = 5;
 
 // Potentiometer
@@ -23,10 +22,11 @@ static const unsigned int potentiometer_ceiling = 960;
 // Tachometer
 static const uint8_t tachometer_pin = 2;
 static const unsigned long counts_per_rev = 4;
-static const unsigned long tachometer_sample_interval = 100;
 unsigned long tachometer_counter = 0;
 unsigned long tachometer_counter_previous = 0;
 unsigned long tachometer_time_previous = 0;
+unsigned long tachometer_counter_sampling_threshold = 20;
+unsigned long tachometer_time_sampling_threshold = 100;
 
 // State variables
 float angular_velocity;
@@ -55,13 +55,6 @@ void setup() {
 void loop() {
   unsigned long new_current_time = millis();
 
-  // Blink
-  if (new_current_time - blink_start_time > blink_period) {
-    blink_state = !blink_state;
-    digitalWrite(LED_BUILTIN, blink_state);
-    blink_start_time = new_current_time;
-  }
-
   // Read the potentiometer
   int potentiometer_value = constrain(analogRead(potentiometer_pin), potentiometer_floor, potentiometer_ceiling);
 
@@ -70,10 +63,11 @@ void loop() {
   analogWrite(motor_pin, motor_pwm);
 
   // Read the tacohmeter
-  const unsigned long tachometer_time_delta = new_current_time - tachometer_time_previous;
-  if (tachometer_time_delta > tachometer_sample_interval) {
-    unsigned long tachometer_counter_current = tachometer_counter;
-    unsigned long tachometer_counter_delta = tachometer_counter_current - tachometer_counter_previous;
+  unsigned long tachometer_counter_current = tachometer_counter;
+  unsigned long tachometer_counter_delta = tachometer_counter_current - tachometer_counter_previous;
+  const unsigned long tachometer_time_delta = millis() - tachometer_time_previous;
+  if ((tachometer_counter_delta > tachometer_counter_sampling_threshold && tachometer_time_delta > 0) || tachometer_time_delta > tachometer_time_sampling_threshold) {
+    digitalWrite(LED_BUILTIN, tachometer_time_delta > tachometer_time_sampling_threshold);
     angular_velocity = 60.0 * 1000 * tachometer_counter_delta / counts_per_rev / tachometer_time_delta; // rpm
     angular_velocity_filter.filter(angular_velocity);
     tachometer_counter_previous = tachometer_counter_current;
